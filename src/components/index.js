dbb039da483a285e2c141af77f04f2267dcfd690
nawaazwill11 <mastermindjim@gmail.com>
2020-07-13 19:35:24 +0530

    bared

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { Layout } from './Layout';
-export { Error } from './Error';
-export { Fresh } from './Fresh';
-export { Main } from './Main';
--- src/config/definitions.js
+++
@@ -1,1 +0,0 @@
-export const extension_id = 'ccicnpdeajgokkadlkfhbanikncnmjmg';
